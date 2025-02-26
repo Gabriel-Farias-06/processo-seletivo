81169fbeb4759e06920908ef4763b0443b24626f
Gabriel-Farias-06 <gabriel.rfarias@a.ucb.br>
2025-02-26 13:33:46 -0300

    termino da funcionalidade Contador

--- src/main/java/DesafioControleFluxo/Contador.java
+++ src/main/java/DesafioControleFluxo/Contador.java
@@ -6,17 +6,22 @@ public class Contador {
     public static void main(String[] args) {
          Scanner terminal = new Scanner(System.in);
          int [] argumentos = new int[2];
+         System.out.println("Insira o 1° valor");
          argumentos[0] = terminal.nextInt();
+         System.out.println("Insira o 2° valor");
          argumentos[1] = terminal.nextInt();
 
          try {
-            contar(argumentos);
-         } catch (Exception exception) {
-
+            int limite = contar(argumentos);
+            for(int i = 0; i != limite; i++)
+                System.out.println(i + 1);
+         } catch (ParametrosInvalidosException exception) {
+            System.out.println("Parâmetros inválidos, o primeiro valor deve ser menor que o segundo");
          }
     }
 
-    public static void contar(int[] args) throws ParametrosInvalidosException{
-        if (args[0] > args[1]) throw ;
+    public static int contar(int[] args) throws ParametrosInvalidosException{
+        if (args[0] > args[1]) throw new ParametrosInvalidosException();
+        return  (args[1] - args[0]);
     }
 }
